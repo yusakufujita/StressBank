f7a03c3cc201a7a8decbc0677e28bdff429a502a
藤田優作 <hinasakun@outlook.jp>
2020-08-01 09:11:55 +0900

    Initial Commit

---
+++ StressBank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StressBank
+//
+//  Created by 藤田優作 on 2020/08/01.
+//  Copyright © 2020 藤田優作. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
